53c8fb96fde1f2b1893a4d30c98e83bb746dc600
Andrey <fi93@mail.ru>
2016-09-27 14:02:56 +0200

    add simple routing

---
+++ src/car/car.component.jsx
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+
+class Car extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Car Page</h1>
+            </div>
+        );
+    };
+}
+
+export default Car;
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,22 @@
 import React, { Component, PropTypes } from 'react';
 import { render } from 'react-dom';
+// Import routing components
+import { Router, Route, Redirect, IndexRedirect, IndexRoute, hashHistory, browserHistory } from 'react-router';
+// Import Custom Components
+import Home from './common/home.component.jsx';
+import About from './common/about.component.jsx';
+import Car from './car/car.component.jsx';
 
 
-class Home extends Component {
 
-    render() {
-        return (
-            <div>
-                <h1>Hi</h1>
-            </div>
-        );
-    };
-}
+render(
+    <Router history={hashHistory}>
+        <Route path="/" component={Home} />
+        <Route path="/car" component={Car} />
+        <Route path="/about" component={About} />
+    </Router>
 
-render(<Home/>, document.getElementById('container'));
+
+
+
+    , document.getElementById('container'));
